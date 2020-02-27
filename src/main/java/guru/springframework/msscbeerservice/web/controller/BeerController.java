56007a6ecf5fc6b921577c8776f8d274dae8615d
KreatorK <vinasy@gmail.com>
2020-02-27 10:08:47 +0200

    adding beer entity. closes #5

--- src/main/java/guru/springframework/msscbeerservice/web/controller/BeerController.java
+++ src/main/java/guru/springframework/msscbeerservice/web/controller/BeerController.java
@@ -1,6 +1,7 @@
 package guru.springframework.msscbeerservice.web.controller;
 
 import guru.springframework.msscbeerservice.web.model.BeerDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,17 +10,20 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/beer")
+@Slf4j
 public class BeerController {
 
     @GetMapping("/{beerId}")
     public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerId) {
         //todo impl
+        log.info("SERVER: getBeerById called!");
         return new ResponseEntity(BeerDto.builder().build(), HttpStatus.OK);
     }
 
     @PostMapping
     public ResponseEntity<BeerDto> saveNewBeer(@RequestBody BeerDto beerDto) {
         //todo impl
+        log.info("SERVER: saveNewBeer called!");
         return new ResponseEntity(HttpStatus.CREATED);
     }
 
@@ -27,6 +31,7 @@ public class BeerController {
     public ResponseEntity<BeerDto> updateBeerById(@PathVariable("beerId") UUID beerId,
                                                   @RequestBody BeerDto beerDto) {
         //todo impl
+        log.info("SERVER: updateBeerById called!");
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 }
