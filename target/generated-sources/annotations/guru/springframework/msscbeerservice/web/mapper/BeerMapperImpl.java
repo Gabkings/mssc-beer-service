925e0cff4a0bf4095497324cc1c35bb71d06dcb0
Gabriel Gitonga <gabriel.gitonga@bskglobaltech.com>
2021-06-03 06:04:14 +0300

    Configuring parent pom to inherite from BOM

--- target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mapper/BeerMapperImpl.java
+++ target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mapper/BeerMapperImpl.java
@@ -6,8 +6,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-06-01T05:03:00+0300",
-    comments = "version: 1.3.1.Final, compiler: javac, environment: Java 14.0.2 (Oracle Corporation)"
+    date = "2021-06-03T06:03:02+0300",
+    comments = "version: 1.3.0.Final, compiler: javac, environment: Java 14.0.2 (Oracle Corporation)"
 )
 @Component
 @Primary
--- target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mapper/BeerMapperImpl_.java
+++ target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mapper/BeerMapperImpl_.java
@@ -12,8 +12,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-06-01T05:02:59+0300",
-    comments = "version: 1.3.1.Final, compiler: javac, environment: Java 14.0.2 (Oracle Corporation)"
+    date = "2021-06-03T06:03:02+0300",
+    comments = "version: 1.3.0.Final, compiler: javac, environment: Java 14.0.2 (Oracle Corporation)"
 )
 @Component
 @Qualifier("delegate")
